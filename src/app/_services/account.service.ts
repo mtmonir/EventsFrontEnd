87d7feb2a3f02734e65cd43cf1e0fa621263b2c2
tahir <tahir.monir@gmail.com>
2021-04-08 18:33:20 -0500

    api added

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -27,7 +27,7 @@ export class AccountService {
     }
 
     login(email: string, password: string) {
-        return this.http.post<any>(`${baseUrl}/authenticate`, { email, password }, { withCredentials: true })
+        return this.http.post<any>(`https://eventfunctionsp2.azurewebsites.net/api/Test/login`, { email, password }, { withCredentials: true })
             .pipe(map(account => {
                 this.accountSubject.next(account);
                 this.startRefreshTokenTimer();
@@ -52,7 +52,7 @@ export class AccountService {
     }
 
     register(account: Account) {
-        return this.http.post(`${baseUrl}/register`, account);
+        return this.http.post(`https://eventfunctionsp2.azurewebsites.net/api/Test/register`, account);
     }
 
     verifyEmail(token: string) {
